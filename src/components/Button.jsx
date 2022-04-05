3a44fb31b42f68810047348b7bdea95fbbd1e5e6
Darko Tomičić <darko.tomicic@CMGC.local>
2022-04-05 08:48:09 +0200

    application done

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Button = ({color, text, onClick}) => {
+  return (
+    <button 
+        style={{backgroundColor: color}} 
+        className='btn'
+        onClick={onClick}
+    >
+        {text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+export default Button
